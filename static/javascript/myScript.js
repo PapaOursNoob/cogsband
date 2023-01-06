8922fd080943276f98bcfefc56e0f8816b48f582
PapaOursNoob <metalgwen@gmail.com>
2023-01-06 16:04:36 +0100

    constructeur

--- static/javascript/myScript.js
+++ static/javascript/myScript.js
@@ -1,4 +1,4 @@
-var ListeBande = (function() {
+var ListeBande = (function () {
   // Méthode privée et propriétés
   cart = [];
 
@@ -8,13 +8,13 @@ var ListeBande = (function() {
     this.compte = compte;
   }
 
-  
+
 })
 
-function myFunction(DIVid) {
-    var myCurrentDiv = document.getElementById(DIVid);
-    var myTitreDiv = document.getElementById("titre");
-    const allFactionNavDiv = document.getElementsByClassName("faction");
+function myFaction(DIVid) {
+  var myCurrentDiv = document.getElementById(DIVid);
+  var myTitreDiv = document.getElementById("titre");
+  const allFactionNavDiv = document.getElementsByClassName("faction");
   if (myCurrentDiv.style.display !== "block") {
     for (let i = 0; i < allFactionNavDiv.length; i++) {
       allFactionNavDiv[i].style.display = "none";
@@ -24,13 +24,13 @@ function myFunction(DIVid) {
   }
 }
 
-function mesReglesIn(DIVid, padding, index){
+function mesReglesIn(DIVid, padding, index) {
   var mesReglesDiv = document.getElementById(DIVid);
   mesReglesDiv.style.display = "block";
   padding = (padding * 5) + (20 + ((index - 1) * 40));
-  mesReglesDiv.style.margin = ''.concat('20px ',padding.toString(),'px') ;
+  mesReglesDiv.style.margin = ''.concat('20px ', padding.toString(), 'px');
 }
-function mesReglesOut(DIVid){
+function mesReglesOut(DIVid) {
   var mesReglesDiv = document.getElementById(DIVid);
   mesReglesDiv.style.display = "none";
 }
